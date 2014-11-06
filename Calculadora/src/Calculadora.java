1aa225c88e3beb2a5fe18b6a711c1948c4f9728b
octopus <octopus@WLAN_conf>
2014-11-06 19:36:38 +0100

    Sóc una calculadora científica

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -2,6 +2,6 @@
 public class Calculadora {
 
 	public static void main(String[] args) {
-		System.out.println("Sóc una calculadora simple");
+		System.out.println("Sóc una calculadora científica");
 	}
 }
